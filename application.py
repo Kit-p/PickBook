2d700724208268d6abc6d3f0f6850bf35a2f7e08
Kit-p <65013409+Kit-p@users.noreply.github.com>
2020-06-08 14:29:51 +0800

    Finished login and logout functionalities

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, g, redirect, render_template, request, session, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,13 +20,36 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+@app.before_request
+def before_request():
+  global db
+  if "user_id" in session:
+    try:
+      g.user
+    except AttributeError:
+      g.user = None
 
-@app.route("/")
+    if g.user is None:
+      users = db.execute("SELECT id, username FROM reader WHERE reader.id = :id", {"id": session["user_id"]})
+      if users.rowcount == 1:
+        user = users.fetchone()
+        g.user = user
+  else:
+    g.user = None
+
+@app.route("/", methods=["GET"])
 def index():
+  if g.user is not None:
+    return redirect(url_for('home'))
   return render_template("index.html")
 
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+  if request.method == "GET":
+    if g.user is not None:
+      return redirect(url_for('home'))
+    return redirect(url_for('index'))
+
   global db
   username = request.form.get("username")
   password = request.form.get("password")
@@ -37,11 +60,39 @@ def register():
   db.commit()
   return render_template("success.html", message="Registration Successful!", username=username)
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-  pass
+  if request.method == "GET":
+    if g.user is not None:
+      return redirect(url_for('home'))
+    return redirect(url_for('index'))
 
-@app.route("/home")
+  if g.user is not None:
+    session.pop("user_id", None)
+  
+  global db
+  username = request.form.get("username")
+  password = request.form.get("password")
+  users = db.execute("SELECT * FROM reader WHERE reader.username = :username", {"username": username})
+  if users.rowcount == 0:
+    return render_template("error.html", message="User does not exist!")
+  elif users.rowcount != 1: # Impossible to happen
+    return render_template("error.html", message="Duplicated users!")
+  else:
+    user = users.fetchone()
+    if user.password != password:
+      return render_template("error.html", message="Incorrect password!")
+    session["user_id"] = user.id
+    return redirect(url_for('home'))
+
+@app.route("/logout", methods=["GET"])
+def logout():
+  if g.user is not None:
+    session.pop("user_id", None)
+  return redirect(url_for('index'))
+
+@app.route("/home", methods=["GET"])
 def home():
-  pass
-
+  if g.user is None:
+    return redirect(url_for('index'))
+  return render_template("success.html", message="Login successful!", username=g.user.username)
